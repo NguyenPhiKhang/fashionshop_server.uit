4521b518e2a037fbe9cafda6227208fb271ff8bb
NguyenPhiKhang <17520616@gm.uit.edu.vn>
2020-07-05 06:58:18 +0700

    fix upload

--- controllers/upload.js
+++ controllers/upload.js
@@ -6,18 +6,22 @@ const uploadFiles = async (req, res) => {
     console.log(req.files);
 
     if (req.files.length <= 0) {
-      return res.send(`You must select at least 1 file.`);
+      res.send({ success: false, message: `You must select at least 1 file.`, filenames: [] });
       // throw new Error("You must select at least 1 file.");
     }
 
-    return res.send(`Files have been uploaded.`);
+    const filenames = await Promise.all(req.files.map(async file =>{
+      return await file.filename;
+    }));
+
+    return res.send({ success: true, message: "Files have been uploaded.", filenames: filenames });
   } catch (error) {
     console.log(error);
 
     if (error.code === "LIMIT_UNEXPECTED_FILE") {
-      return res.send("Too many files to upload.");
+      return res.send({ success: false, message: "Too many files to upload.", filenames: [] });
     }
-    return res.send(`Error when trying upload many files: ${error}`);
+    return res.send({ success: false, message: `Error when trying upload many files: ${error}`, filenames: [] });
   }
 };
 
